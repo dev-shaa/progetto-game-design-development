f46e0e77989b7ae25102bc555e09eb595b8d38e9
Trincalex <al.trincone@studenti.unina.it>
2024-12-19 18:23:40 +0100

    Introdotti ButtonInputComponent e ButtonRenderComponent

---
+++ app/src/main/java/unina/game/myapplication/logic/ButtonInputComponent.java
@@ -0,0 +1,19 @@
+package unina.game.myapplication.logic;
+
+import com.badlogic.androidgames.framework.Input;
+
+import unina.game.myapplication.core.InputComponent;
+
+public class ButtonInputComponent extends InputComponent {
+
+    boolean isPressed = false;
+    public ButtonRenderComponent buttonRenderComponent;
+
+    @Override
+    public void process(Input input) {
+        if (input.isKeyPressed(0)) {
+            isPressed = !isPressed;
+            //buttonRenderComponent.render();
+        }
+    }
+}

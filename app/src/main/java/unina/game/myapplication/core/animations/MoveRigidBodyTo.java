0c2f0b3297930ee86d7a1085e4f01c8d19f1ec42
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-02-07 12:18:30 +0100

    Implement EaseFunction interface as non-inner class

--- app/src/main/java/unina/game/myapplication/core/animations/MoveRigidBodyTo.java
+++ app/src/main/java/unina/game/myapplication/core/animations/MoveRigidBodyTo.java
@@ -2,27 +2,18 @@ package unina.game.myapplication.core.animations;
 
 import com.badlogic.androidgames.framework.Pool;
 
-import unina.game.myapplication.core.GameObject;
 import unina.game.myapplication.core.Utility;
 import unina.game.myapplication.core.physics.RigidBody;
 
-public class MoveRigidBodyTo implements Animation{
-    public interface EaseFunction {
-
-        MoveRigidBodyTo.EaseFunction LINEAR = x -> Utility.clamp(0, 1, x);
-        MoveRigidBodyTo.EaseFunction CUBIC_IN_OUT = x -> x < 0.5 ? 4 * x * x * x : (float) (1 - Math.pow(-2 * x + 2, 3) / 2);
-
-        float evaluate(float x);
-
-    }
+public class MoveRigidBodyTo implements Animation {
 
     private static Pool<MoveRigidBodyTo> pool;
 
     public static MoveRigidBodyTo build(RigidBody rigidBody, float targetX, float targetY, float duration) {
-        return build(rigidBody, targetX, targetY, duration, MoveRigidBodyTo.EaseFunction.LINEAR);
+        return build(rigidBody, targetX, targetY, duration, EaseFunction.LINEAR);
     }
 
-    public static MoveRigidBodyTo build(RigidBody rigidBody, float targetX, float targetY, float duration, MoveRigidBodyTo.EaseFunction easeFunction) {
+    public static MoveRigidBodyTo build(RigidBody rigidBody, float targetX, float targetY, float duration, EaseFunction easeFunction) {
         if (pool == null)
             pool = new Pool<>(MoveRigidBodyTo::new, 8);
 
@@ -39,7 +30,7 @@ public class MoveRigidBodyTo implements Animation{
     private float startX, startY;
     private float targetX, targetY;
     private float duration;
-    private MoveRigidBodyTo.EaseFunction easeFunction = MoveRigidBodyTo.EaseFunction.LINEAR;
+    private EaseFunction easeFunction;
     private float current;
 
     private MoveRigidBodyTo() {
@@ -67,7 +58,7 @@ public class MoveRigidBodyTo implements Animation{
         float x = Utility.lerp(startX, targetX, t);
         float y = Utility.lerp(startY, targetY, t);
 
-        rigidBody.setTransform(x,y);
+        rigidBody.setTransform(x, y);
 
 //        rigidBody.x = Utility.lerp(startX, targetX, t);
 //        rigidBody.y = Utility.lerp(startY, targetY, t);

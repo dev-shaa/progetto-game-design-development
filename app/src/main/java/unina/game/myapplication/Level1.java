d08fd4ac3056eaec2033ff1f4d6b1b4008632290
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-02-21 21:37:26 +0100

    Complete Level 1

--- app/src/main/java/unina/game/myapplication/Level1.java
+++ app/src/main/java/unina/game/myapplication/Level1.java
@@ -20,6 +20,7 @@ import unina.game.myapplication.logic.common.RectRenderer;
 import unina.game.myapplication.logic.common.DottedLineRenderer;
 import unina.game.myapplication.logic.common.FadeAnimation;
 import unina.game.myapplication.logic.common.Button;
+import unina.game.myapplication.logic.menu.MainMenu;
 
 public class Level1 extends Scene {
 
@@ -28,6 +29,7 @@ public class Level1 extends Scene {
 
     private Pixmap backgroundImage;
     private Pixmap elementsImage;
+    private Pixmap elementsUIImage;
 
     private Sound buttonSound;
     private Sound buttonsAppearSound;
@@ -46,8 +48,11 @@ public class Level1 extends Scene {
 
         Camera.getInstance().setSize(10);
 
-        backgroundImage = game.getGraphics().newPixmap("graphics/background-level1.png", Graphics.PixmapFormat.ARGB8888);
-        elementsImage = game.getGraphics().newPixmap("graphics/elements-light.png", Graphics.PixmapFormat.ARGB8888);
+        setClearColor(PALETTE_BACKGROUND);
+
+        backgroundImage = game.getGraphics().newPixmap("graphics/environment-roundabout.png", Graphics.PixmapFormat.RGB565);
+        elementsImage = game.getGraphics().newPixmap("graphics/elements-light.png", Graphics.PixmapFormat.RGB565);
+        elementsUIImage = game.getGraphics().newPixmap("graphics/elements-ui.png", Graphics.PixmapFormat.RGB565);
 
         buttonSound = game.getAudio().newSound("sounds/kenney-interface-sounds/click_002.ogg");
         buttonsAppearSound = game.getAudio().newSound("sounds/kenney-ui-sounds/switch4.ogg");
@@ -55,31 +60,19 @@ public class Level1 extends Scene {
         winSound = game.getAudio().newSound("sounds/kenney-sax-jingles/jingles_SAX10.ogg");
 
         // Background
-        GameObject backgroundGO = createGameObject();
-
-        SpriteRenderer backgroundRenderer = backgroundGO.addComponent(SpriteRenderer.class);
+        SpriteRenderer backgroundRenderer = createGameObject().addComponent(SpriteRenderer.class);
         backgroundRenderer.setImage(backgroundImage);
-        backgroundRenderer.setSize(10, 10 / backgroundImage.getAspectRatio());
-        backgroundRenderer.setLayer(-10);
+        backgroundRenderer.setSize(Camera.getInstance().getSizeX(), Camera.getInstance().getSizeY());
+        backgroundRenderer.setLayer(16);
 
-        // Rectangle to hide the character when it enters the door
-        GameObject rectRendererGO = createGameObject(3.4f, -1.25f);
-
-        RectRenderer rectRenderer = rectRendererGO.addComponent(RectRenderer.class);
-        rectRenderer.setPivot(0, 1);
-        rectRenderer.setSize(1, 2);
-        rectRenderer.setColor(PALETTE_BACKGROUND);
-        rectRenderer.setLayer(10);
-
-        // Transition
-        GameObject fade = createGameObject();
-        RectRenderer fullScreenRenderer = fade.addComponent(RectRenderer.class);
+        // Transition Panel
+        RectRenderer fullScreenRenderer = createGameObject().addComponent(RectRenderer.class);
         fullScreenRenderer.setSize(50, 50);
         fullScreenRenderer.setLayer(Integer.MAX_VALUE);
         fullScreenRenderer.setColor(Color.TRANSPARENT);
 
         // Bridge
-        GameObject bridge = createGameObject(-2.5f, -1.3f);
+        GameObject bridge = createGameObject(-2.5f, -1.35f);
 
         SpriteRenderer bridgeRenderComponent = bridge.addComponent(SpriteRenderer.class);
         bridgeRenderComponent.setImage(elementsImage);
@@ -87,32 +80,32 @@ public class Level1 extends Scene {
         bridgeRenderComponent.setSrcSize(128, 32);
         bridgeRenderComponent.setSize(2.2f, 2f / 4f);
         bridgeRenderComponent.setPivot(0.1f, 0f);
-        bridgeRenderComponent.setLayer(-3);
-
+        bridgeRenderComponent.setLayer(0);
 
         // Character
-        GameObject character = createGameObject(-43f, -1.25f);
+        GameObject character = createGameObject(-43f, -1.3f);
+
         SpriteRenderer characterRenderer = character.addComponent(SpriteRenderer.class);
         characterRenderer.setImage(elementsImage);
         characterRenderer.setSize(1, 1);
         characterRenderer.setSrcPosition(128, 128);
         characterRenderer.setSrcSize(128, 128);
         characterRenderer.setPivot(0.5f, 1f);
-        characterRenderer.setLayer(8);
+        characterRenderer.setLayer(0);
 
         // Button
-        CircleRenderer circleRenderer = createGameObject(-1, 5).addComponent(CircleRenderer.class);
+        CircleRenderer circleRenderer = createGameObject(-1, 2.5f).addComponent(CircleRenderer.class);
         circleRenderer.setRadius(0.75f);
         circleRenderer.setColor(PALETTE_PRIMARY);
 
-        GameObject button = createGameObject(-1f, 5f);
+        GameObject button = createGameObject(-1f, 2.5f);
 
         SpriteRenderer buttonRenderComponent = button.addComponent(SpriteRenderer.class);
         buttonRenderComponent.setImage(elementsImage);
         buttonRenderComponent.setSize(2.5f, 2.5f);
         buttonRenderComponent.setSrcPosition(0, 0);
         buttonRenderComponent.setSrcSize(128, 128);
-        buttonRenderComponent.setLayer(3);
+        buttonRenderComponent.setLayer(0);
 
         Button buttonInputComponent = button.addComponent(Button.class);
         buttonInputComponent.setSize(3, 3);
@@ -122,32 +115,34 @@ public class Level1 extends Scene {
         DottedLineRenderer lineRenderer = lineRendererGO.addComponent(DottedLineRenderer.class);
         lineRenderer.setPointA(button.x, button.y - 1.25f);
         lineRenderer.setPointB(-1f, bridge.y);
-        lineRenderer.setCount(16);
+        lineRenderer.setCount(8);
         lineRenderer.setRadius(0.1f);
         lineRenderer.setColor(Color.GREY);
         lineRenderer.setLayer(-4);
 
         // Level selection button
-        GameObject menuButtonGO = createGameObject(-4, Camera.getInstance().getSizeY() / 2 + 4);
+        GameObject menuButtonGO = createGameObject(-20, 9);
 
-        RectRenderer menuButtonRenderer = menuButtonGO.addComponent(RectRenderer.class);
-        menuButtonRenderer.setSize(2f, 2);
-        menuButtonRenderer.setColor(Color.GREEN);
-        menuButtonRenderer.setLayer(10);
-        menuButtonRenderer.setPivot(0f, 0f);
+        SpriteRenderer menuButtonRenderer = menuButtonGO.addComponent(SpriteRenderer.class);
+        menuButtonRenderer.setImage(elementsUIImage);
+        menuButtonRenderer.setSrcPosition(0, 0);
+        menuButtonRenderer.setSrcSize(128, 128);
+        menuButtonRenderer.setSize(3f, 3f);
+        menuButtonRenderer.setLayer(28);
+        menuButtonRenderer.setPivot(0.5f, 0.5f);
 
         Button menuButton = menuButtonGO.addComponent(Button.class);
-        menuButton.setSize(3, 3);
+        menuButton.setSize(6, 3);
         menuButton.setInteractable(false);
         menuButton.setOnClick(() -> {
             menuButton.setInteractable(false);
             buttonInputComponent.setInteractable(false);
+            buttonSound.play(1);
 
             animator.clear();
-            animator.add(FadeAnimation.build(fullScreenRenderer, Color.TRANSPARENT, Color.BLACK, 0.75f), () -> loadScene(Level1.class));
+            animator.add(FadeAnimation.build(fullScreenRenderer, Color.TRANSPARENT, Color.BLACK, 0.75f), () -> loadScene(MainMenu.class));
             animator.start();
         });
-
 
         buttonInputComponent.setOnClick(() -> {
             buttonSound.play(1);
@@ -177,7 +172,7 @@ public class Level1 extends Scene {
                         MoveToAnimation.build(character, -2.5f, character.y, 0.25f, EaseFunction.CUBIC_IN_OUT)
                 ),
                 () -> characterRenderer.setSrcPosition(0, 128));
-        animator.add(MoveToAnimation.build(menuButtonGO, menuButtonGO.x, Camera.getInstance().getSizeY() / 2, 0.25f, EaseFunction.CUBIC_IN_OUT),
+        animator.add(MoveToAnimation.build(menuButtonGO, -Camera.getInstance().getSizeX() / 2 + 1.5f, menuButtonGO.y, 0.25f, EaseFunction.CUBIC_IN_OUT),
                 () -> {
                     buttonInputComponent.interactable = true;
                     menuButton.interactable = true;
@@ -191,6 +186,7 @@ public class Level1 extends Scene {
 
         backgroundImage.dispose();
         elementsImage.dispose();
+        elementsUIImage.dispose();
 
         buttonSound.dispose();
         buttonsAppearSound.dispose();
